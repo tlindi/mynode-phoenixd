1d1ee2b5b95b03d7bd5abe69f645e774d1e8d076
tlindi <tomi@lind.fi>
2025-04-23 14:23:57 +0300

    update scripts/install_phoenixd.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # uncomment to disable build caching
-#export CACHEBUST=$(date +%s)
+export CACHEBUST=$(date +%s)
 echo if not empty, CACHING disabled $CACHEBUST
 cp -av ../Dockerfile .docker/ && bash ../scripts/install_phoenixd.sh
