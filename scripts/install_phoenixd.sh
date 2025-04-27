e6e067d08f40819cd42db6bfa8604ed4b208c6d3
tlindi <tomi@lind.fi>
2025-04-27 10:12:32 +0300

    Implement UIDGID setup on pre-service

--- scripts/install_phoenixd.sh
+++ scripts/install_phoenixd.sh
@@ -22,66 +22,45 @@ mkdir -p /opt/mynode/phoenixd || true
 docker pull acinq/phoenixd:${VERSION}
 docker tag acinq/phoenixd:${VERSION} phoenixd
 
-# create data dir and restore latest found backup
+## create data dir
 #
 export PHOENIXD_DATA_DIR=/mnt/hdd/mynode/phoenixd
 if [ ! -d "$PHOENIXD_DATA_DIR" ]; then
-    mkdir -p "$PHOENIXD_DATA_DIR" || { echo "Failed to create $PHOENIXD_DATA_DIR"; exit 1; }
-    echo "Successfully created $PHOENIXD_DATA_DIR"
+    mkdir -p "$PHOENIXD_DATA_DIR" || { echo "Failed to create data directory: $PHOENIXD_DATA_DIR"; exit 1; }
+    echo "Successfully created data directory:"
+	echo "$PHOENIXD_DATA_DIR"
 fi
 
-echo "Running UID is: $(id)"
-###
-#  TEXT vs NUMBER UID/GID issue could (maybe) be solved with docker-compose
-###
-## Verify and fix ownership if necessary (TEXT)
-#CURRENT_OWNER=$(stat -c '%U' "$PHOENIXD_DATA_DIR")
-#CURRENT_GROUP=$(stat -c '%G' "$PHOENIXD_DATA_DIR")
-## Verify and fix ownership if necessary (number)
-CURRENT_OWNER=$(stat -c '%u' "$PHOENIXD_DATA_DIR")
-CURRENT_GROUP=$(stat -c '%g' "$PHOENIXD_DATA_DIR")
-
-## TEXT 
-#if [ "$CURRENT_OWNER" != "bitcoin" ] || [ "$CURRENT_GROUP" != "bitcoin" ]; then
-## Number 
-if [ "$CURRENT_OWNER" != "1000" ] || [ "$CURRENT_GROUP" != "1000" ]; then
-    echo "Incorrect ownership detected ($CURRENT_OWNER:$CURRENT_GROUP). Updating to bitcoin:bitcoin..."
-## TEXT
-#    chown bitcoin:bitcoin "$PHOENIXD_DATA_DIR" || { echo "Failed to set ownership for $PHOENIXD_DATA_DIR"; exit 1; }
-    chown 1000:1000 "$PHOENIXD_DATA_DIR" || { echo "Failed to set owner:group for $PHOENIXD_DATA_DIR"; exit 1; }
-## TEXT
-#    echo "Ownership successfully updated to bitcoin:bitcoin."
-    echo "Ownership successfully updated to 1000:1000."
-else
-## TEXT
-#    echo "Ownership is already correct: bitcoin:bitcoin."
-    echo "Ownership is already correct: 1000:1000."
-fi
-echo "Continue installation..."
+##  restore latest found backup
+#
+echo "Populate data dir from backup, if such exists."
 
 export PHOENIXD_BACKUP_DIR=/mnt/hdd/mynode/phoenixd_backup
-if [ -d "$PHOENIXD_BACKUP_DIR" ]; then
+if [ ! -d "$PHOENIXD_BACKUP_DIR" ]; then
+    echo "Previous backup directory not found at:"
+	echo "$PHOENIXD_BACKUP_DIR so creating it..."
+    mkdir -p "$PHOENIXD_BACKUP_DIR" || { echo "Backup path '$PHOENIXD_BACKUP_DIR' couldn't be created."; exit 1; }
+#    chown bitcoin:bitcoin "$PHOENIXD_BACKUP_DIR" || { echo "Backup path owner couldn't be set."; exit 1; }
+    chown 1000:1000 "$PHOENIXD_BACKUP_DIR" || { echo "Backup path owner:group couldn't be set."; exit 1; }
+        
+	echo "phoenixd "$VERSION" will open a new wallet."
+else
+    echo "Existing backup directory found:"
+	echo "$PHOENIXD_BACKUP_DIR"
     # Capture backup files, supress error output if none are found, and give name of nost recent
     export BACKUP_FILE=$(ls -1 "$PHOENIXD_BACKUP_DIR"/*.tar.gz 2>/dev/null | \
         sed -E 's/.*(.{15})\.tar\.gz$/\1|\0/' | \
         sort | tail -1 | cut -d'|' -f2-)
 
     if [ -n "$BACKUP_FILE" ]; then
-        echo "Restoring latest found backup: $BACKUP_FILE"
+        echo "Restoring from latest backup file found:"
+		echo "$BACKUP_FILE"
         tar xzvf "$BACKUP_FILE" --strip-components=1 -C "$PHOENIXD_DATA_DIR"
     else
-        echo "No restoreable backup was found. Starting with a new phoenixd wallet."
+        echo "No restoreable backup was found."
     fi
-else
-    echo "Backup directory $PHOENIXD_BACKUP_DIR does not exist. Creating..."
-    if [ ! -d "$BACKUP_PATH" ]; then 
-        mkdir -p "$BACKUP_PATH" || { echo "Backup path '$BACKUP_PATH' couldn't be created."; exit 1; }
-#        chown bitcoin:bitcoin "$BACKUP_PATH" || { echo "Backup path owner couldn't be set."; exit 1; }
-        chown 1000:1000 "$BACKUP_PATH" || { echo "Backup path owner:group couldn't be set."; exit 1; }
-    fi
-        # save installed version info to backup purposes
-	echo "$VERSION" > "$PHOENIXD_BACKUP_DIR\phoenixd_version"
-	echo "Starting phoenixd wallet "$VERSION" and creating new wallet configuration."
 fi
+
+echo $VERSION > "$PHOENIXD_BACKUP_DIR/phoenixd_version"
 
 echo "================== DONE INSTALLING APP ================="
